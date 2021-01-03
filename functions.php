0d4aaa09350d56ef9e0cc45fe0be6d5465458ba7
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-01-03 16:29:14 -0300

    trying to make themme from beginning

--- functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-    function load_stylesheets() {
-        wp_register_style('style', get_template_directory_uri() . './style.css', array(), false, 'all');
-        wp_enqueue_style('style');
-    }
-
-    load_stylesheets();
-    add_theme_support('menu');
-?>
--- index.php
+++ index.php
@@ -1,4 +1,37 @@
-<?php get_header(); ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <link href="./style.css" rel="stylesheet">
 
-<?php get_footer(); ?>
+        <?php wp_head(); ?>
+    </head>
+    <body>
+        <h1><?php bloginfo('name'); ?></h1>
+        <h2><?php bloginfo('description'); ?></h2>
+
+        <?php
+        if( have_posts() ) :
+            while( have_posts() ):
+                the_post();
+                ?>
+
+                <h3><?php the_title(); ?></h3>
+                <div>
+                    <?php the_content(); ?>
+                </div>
+        
+                <?php
+            endwhile;
+        endif;
+        ?>
+        
+
+        <?php wp_footer(); ?>
+    </body>
+</html>
+
