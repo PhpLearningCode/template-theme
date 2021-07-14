1dfd2fd45c0b82cac4c11120e5785bf1a0f7c833
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-07-14 09:31:28 -0300

    working on theme

--- header.php
+++ header.php
@@ -7,7 +7,13 @@
 <body <?php body_class()?> >
 
 <header class="sticky-top">
-    <div class="container">
+    <div id="nav_menu">
+        <img
+            alt="" 
+            src="<?php header_image(); ?>" 
+            width="<?php echo get_custom_header()->width; ?>" 
+            height="<?php echo get_custom_header()->height; ?>"
+        />
         <?php wp_nav_menu(
             array(
                 'theme_location' => 'top-menu',
