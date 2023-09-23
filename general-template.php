5a7ff898f71441d5f3074548e1722c4bc860f920
williamguilhermesouza <williamguilhermesouza@gmail.com>
2023-09-23 17:16:43 -0300

    created a general template

---
+++ general-template.php
@@ -0,0 +1,38 @@
+<?php 
+/*
+Template Name: General Template
+*/
+?>
+
+<?php get_header() ?>
+    <div class="content-area">
+        <main>
+            <section class="middle-area">
+                <div class="container">
+                    <div class="general-template">
+                        <!-- First part of Mainloop -->
+                        <?php 
+                            if (have_posts(  )){
+                                while( have_posts(  )){
+                                    the_post(  );
+                                    ?>
+                        <!-- Using the retrieved data of Mainloop at the Wordpress Site -->
+                                    <article>
+                                        <h2><?php the_title( ); ?></h2>
+                                        <?php the_content( ); ?>
+                                    </article>
+                        <!-- Opening php tag again to finish the loop with an else, showing a p if there are no posts -->                    
+                                    <?php 
+                                }
+                            }
+                            else {
+                        ?>
+                            <p>There's nothing yet to be displayed...</p>
+                        <?php } ?>
+                        <!-- End of Mainloop -->
+                    </div>
+                </div>
+            </section>
+        </main>
+    </div>
+    <?php get_footer() ?>
---
+++ page-home.php
@@ -0,0 +1,42 @@
+<?php get_header() ?>
+    <div class="content-area">
+        <main>
+            <section class="carousel">
+                <div class="container">
+                    <div class="row">Carousel</div>
+                </div>
+            </section>
+            <section class="middle-area">
+                <div class="container">
+                    <div class="row">
+                        <aside class="sidebar col-md-4">Sidebar</aside>
+                        <div class="news col-md-8">
+                            <!-- First part of Mainloop -->
+                            <?php 
+                                if (have_posts(  )){
+                                    while( have_posts(  )){
+                                        the_post(  );
+                                        ?>
+                            <!-- Using the retrieved data of Mainloop at the Wordpress Site -->
+                                       <p>Arquivo Home.php</p>
+                            <!-- Opening php tag again to finish the loop with an else, showing a p if there are no posts -->                    
+                                        <?php 
+                                    }
+                                }
+                                else {
+                            ?>
+                                <p>There's nothing yet to be displayed...</p>
+                            <?php } ?>
+                            <!-- End of Mainloop -->
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="map">
+                <div class="container">
+                    <div class="row">Mapa</div>
+                </div>
+            </section>
+        </main>
+    </div>
+    <?php get_footer() ?>
