73bbbabafba9170c83159d81b27e40c03a2a402d
williamguilhermesouza <williamguilhermesouza@gmail.com>
2023-09-27 21:46:21 -0300

    included 404 and archive pages

---
+++ archive.php
@@ -0,0 +1,38 @@
+<?php get_header() ?>
+    <img class="img-responsive" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="header-image" />
+    <div class="content-area">
+        <main>
+            <section class="middle-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="archive col-md-8">
+                            <!-- First part of Mainloop -->
+                            <?php 
+                                the_archive_title( '<h1 class="archive-title">', '</h1>' );
+                                the_archive_description();
+                                if (have_posts(  )){
+                                    while( have_posts(  )){
+                                        the_post(  );
+                            // Using the retrieved data of Mainloop at the Wordpress Site while calling a template part
+                                        get_template_part( 'template-parts/content', 'archive' ); 
+                                    }
+                                    ?>
+                                    <div class="row">
+                                        <div class="pages text-start col-6"><?php previous_posts_link( "<< Newer posts" ); ?></div>
+                                        <div class="pages text-end col-6"><?php next_posts_link( "Older posts >>" ) ?></div>
+                                    </div>
+                                    <?php
+                                }
+                                else {
+                            ?>
+                                <p>There's nothing yet to be displayed...</p>
+                            <?php } ?>
+                            <!-- End of Mainloop -->
+                        </div>
+                        <?php get_sidebar( 'blog' ); ?>
+                    </div>
+                </div>
+            </section>
+        </main>
+    </div>
+    <?php get_footer() ?>
